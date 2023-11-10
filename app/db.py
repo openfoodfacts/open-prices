c3e52ec62aebca5d99d9e2e345c227c23caae1d7
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-10 01:40:45 +0100

    Setup DB

---
+++ app/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+
+from app.config import settings
+
+
+url = URL.create(
+    drivername="postgresql",
+    database=settings.postgres_db_name,
+    username=settings.postgres_user,
+    password=settings.postgres_password,
+    host=settings.postgres_host,
+    port=settings.postgres_port,
+)
+
+engine = create_engine(url)
+
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
