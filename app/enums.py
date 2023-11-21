0895125dd4384e562fd69744d4275ea7ea7d5c6d
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-21 13:48:02 +0100

    Add Product model

--- app/enums.py
+++ app/enums.py
@@ -1,6 +1,13 @@
 from enum import Enum
 
 
+class ProductOFFSource(Enum):
+    OFF = "OFF"  # Open Food Facts
+    OPF = "OPF"  # Open Products Facts
+    OPFF = "OPFF"  # Open Pet Food Facts
+    OBF = "OBF"  # Open Beauty Facts
+
+
 class LocationOSMType(Enum):
     NODE = "NODE"
     WAY = "WAY"
--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ from sqlalchemy_utils.types.choice import ChoiceType
 from sqlalchemy_utils.types.currency import CurrencyType
 
 from app.db import Base
-from app.enums import LocationOSMType
+from app.enums import LocationOSMType, ProductOFFSource
 
 force_auto_coercion()
 
@@ -31,12 +31,27 @@ class User(Base):
     __tablename__ = "users"
 
 
+class Product(Base):
+    id = Column(Integer, primary_key=True, index=True)
+
+    code = Column(String, unique=True, index=True)
+
+    off_source = Column(ChoiceType(ProductOFFSource))
+    off_name = Column(String)
+    off_quantity = Column(Integer)
+    off_image_url = Column(String)
+
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+
+    __tablename__ = "products"
+
+
 class Location(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     osm_id = Column(BigInteger)
     osm_type = Column(ChoiceType(LocationOSMType))
-
     osm_name = Column(String)
     osm_display_name = Column(String)
     osm_address_postcode = Column(String)
