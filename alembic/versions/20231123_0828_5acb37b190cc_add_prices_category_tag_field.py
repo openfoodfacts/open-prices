7634ee788e71e5122d3ea70333cda4bc08760513
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-23 10:50:08 +0100

    build: fix CI (linting of migration file) (#59)

--- alembic/versions/20231123_0828_5acb37b190cc_add_prices_category_tag_field.py
+++ alembic/versions/20231123_0828_5acb37b190cc_add_prices_category_tag_field.py
@@ -7,9 +7,9 @@ Create Date: 2023-11-23 08:28:39.136672
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "5acb37b190cc"
