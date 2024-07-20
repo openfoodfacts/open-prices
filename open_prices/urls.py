216f72baca600e6248476592e5b7261593d6f774
Raphaël Odini <raphodn@users.noreply.github.com>
2024-07-20 12:54:51 +0200

    feat(Django): init setup (#362)

---
+++ open_prices/urls.py
@@ -0,0 +1,12 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("www.urls")),
+]
+
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
+    urlpatterns += debug_toolbar_urls()
