7b40cd20cf4d4aab2170918cc8881fce6122a229
Raphael Odini <raphodn@users.noreply.github.com>
2024-07-23 13:54:39 +0200

    Status view

--- open_prices/api/urls.py
+++ open_prices/api/urls.py
@@ -1,3 +1,22 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+from open_prices.api.views import StatusView
+
 app_name = "api"
 
-urlpatterns = []
+urlpatterns = [
+    path("v1/status", StatusView.as_view(), name="status"),
+    # Swagger / OpenAPI documentation
+    path("v1/schema", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "v1/docs",
+        SpectacularSwaggerView.as_view(url_name="api:schema"),
+        name="swagger-ui",
+    ),
+    path("v1/redoc", SpectacularRedocView.as_view(url_name="api:schema"), name="redoc"),
+]
