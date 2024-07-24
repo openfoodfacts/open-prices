36c120b9585379464659bacf9ce65ff0e23a78a0
Raphael Odini <raphodn@users.noreply.github.com>
2024-07-24 19:24:38 +0200

    Add ordering, filtering, pagination

--- open_prices/api/users/views.py
+++ open_prices/api/users/views.py
@@ -1,5 +1,7 @@
-from rest_framework import mixins, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
 
+from open_prices.api.users.filters import UserFilter
 from open_prices.api.users.serializers import UserSerializer
 from open_prices.users.models import User
 
@@ -7,3 +9,6 @@ from open_prices.users.models import User
 class UserViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.has_prices().all()
     serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = UserFilter
+    ordering_fields = ["price_count"]
