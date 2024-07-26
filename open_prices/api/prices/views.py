2a70d125b544c92445027f36b9a6f44b88177ec5
Raphaël Odini <raphodn@users.noreply.github.com>
2024-07-26 17:35:50 +0200

    feat(Django): API: Price PATCH & DELETE endpoints (#372)

--- open_prices/api/prices/views.py
+++ open_prices/api/prices/views.py
@@ -8,25 +8,39 @@ from open_prices.api.prices.filters import PriceFilter
 from open_prices.api.prices.serializers import (
     PriceCreateSerializer,
     PriceFullSerializer,
+    PriceUpdateSerializer,
 )
 from open_prices.common.authentication import CustomAuthentication
 from open_prices.prices.models import Price
 
 
 class PriceViewSet(
-    mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
 ):
     authentication_classes = [CustomAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+    http_method_names = ["get", "post", "patch", "delete"]  # disable "put"
     queryset = Price.objects.all()
     serializer_class = PriceFullSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = PriceFilter
-    ordering_fields = ["date", "created"]
+    ordering_fields = ["price", "date", "created"]
+
+    def get_queryset(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            # Only return prices owned by the current user
+            return Price.objects.filter(owner=self.request.user.user_id)
+        return self.queryset
 
     def get_serializer_class(self):
         if self.request.method == "POST":
             return PriceCreateSerializer
+        elif self.request.method == "PATCH":
+            return PriceUpdateSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
