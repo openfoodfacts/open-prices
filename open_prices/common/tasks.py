6ad8fa63927b51239583c8be5d629725e80d2d37
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-05 22:47:53 +0200

    Working script

--- open_prices/common/tasks.py
+++ open_prices/common/tasks.py
@@ -1,8 +1,21 @@
+import gzip
+import json
+import os
+from pathlib import Path
+
+import tqdm
+from django.conf import settings
 from django_q.models import Schedule
 from django_q.tasks import schedule
 from openfoodfacts import Flavor
 
+from open_prices.api.locations.serializers import LocationSerializer
+from open_prices.api.prices.serializers import PriceSerializer
+from open_prices.api.proofs.serializers import ProofSerializer
 from open_prices.common.openfoodfacts import import_product_db
+from open_prices.locations.models import Location
+from open_prices.prices.models import Price
+from open_prices.proofs.models import Proof
 
 
 def import_product_db_task():
@@ -11,7 +24,18 @@ def import_product_db_task():
 
 
 def dump_db_task():
-    pass
+    output_dir = Path(os.path.join(settings.BASE_DIR, "static/data"))
+
+    for table_name, model_cls, schema_cls in (
+        ("prices", Price, PriceSerializer),
+        ("proofs", Proof, ProofSerializer),
+        ("locations", Location, LocationSerializer),
+    ):
+        output_path = output_dir / f"{table_name}.jsonl.gz"
+        with gzip.open(output_path, "wt") as f:
+            for item in tqdm.tqdm(model_cls.objects.all(), desc=table_name):
+                f.write(json.dumps(schema_cls(item).data))
+                f.write("\n")
 
 
 # sync product database with Open Food Facts daily at 15:00
