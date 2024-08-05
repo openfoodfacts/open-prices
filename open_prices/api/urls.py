606119abe69b23bb0690b23e74ccfb59b54f66f9
Raphaël Odini <raphodn@users.noreply.github.com>
2024-08-05 14:42:23 +0200

    refactor(Django): improve API documentation (#379)

--- open_prices/api/urls.py
+++ open_prices/api/urls.py
@@ -15,7 +15,7 @@ from open_prices.api.views import StatusView
 
 app_name = "api"
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"v1/users", UserViewSet, basename="users")
 router.register(r"v1/locations", LocationViewSet, basename="locations")
 router.register(r"v1/products", ProductViewSet, basename="products")
@@ -25,15 +25,15 @@ router.register(r"v1/prices", PriceViewSet, basename="prices")
 urlpatterns = [
     path("v1/auth/", include("open_prices.api.auth.urls")),
     # health check
-    path("v1/status", StatusView.as_view(), name="status"),
+    path("status", StatusView.as_view(), name="status"),
     # Swagger / OpenAPI documentation
-    path("v1/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("schema", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "v1/docs",
+        "docs",
         SpectacularSwaggerView.as_view(url_name="api:schema"),
         name="swagger-ui",
     ),
-    path("v1/redoc", SpectacularRedocView.as_view(url_name="api:schema"), name="redoc"),
+    path("redoc", SpectacularRedocView.as_view(url_name="api:schema"), name="redoc"),
 ]
 
 urlpatterns += router.urls
