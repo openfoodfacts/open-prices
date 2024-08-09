1386fa62be3aa5fcba2f55506475e979580f2a68
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-09 09:20:12 +0200

    Add tests

--- open_prices/common/tests.py
+++ open_prices/common/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from open_prices.common.utils import is_float
+from open_prices.common.utils import is_float, truncate_decimal
 
 
 class UtilsTest(TestCase):
@@ -13,3 +13,10 @@ class UtilsTest(TestCase):
             self.assertTrue(is_float(PRICE_OK))
         for PRICE_NOT_OK in ["", " ", "a", "1,5", "1.5.0"]:
             self.assertFalse(is_float(PRICE_NOT_OK))
+
+    def test_truncate_decimal(self):
+        self.assertEqual(truncate_decimal("0.1234567"), "0.1234567")
+        self.assertEqual(truncate_decimal("0.123456789"), "0.1234567")
+        self.assertEqual(
+            truncate_decimal("0.123456789", max_decimal_places=9), "0.123456789"
+        )
