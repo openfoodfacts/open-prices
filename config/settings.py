52f0803d6b34af5f679a0038d98203eb70bd653d
Raphael Odini <raphodn@users.noreply.github.com>
2024-08-12 20:06:48 +0200

    temp

--- config/settings.py
+++ config/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 
 
 # Default primary key field type
