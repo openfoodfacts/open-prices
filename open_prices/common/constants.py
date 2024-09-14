fdd4c7b95980fa43727dbe440f834793665878e9
Raphael Odini <raphodn@users.noreply.github.com>
2024-09-14 23:42:04 +0200

    fix: fix babel currency list order

--- open_prices/common/constants.py
+++ open_prices/common/constants.py
@@ -1,4 +1,4 @@
 from babel.numbers import list_currencies
 
-CURRENCY_LIST = [currency.upper() for currency in list_currencies()]
+CURRENCY_LIST = sorted(list_currencies())
 CURRENCY_CHOICES = [(key, key) for key in CURRENCY_LIST]
