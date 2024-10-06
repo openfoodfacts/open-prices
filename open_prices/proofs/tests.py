e318b794aad85e599b19b73ee1032ae44752982f
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-06 18:51:32 +0200

    Add tests

--- open_prices/proofs/tests.py
+++ open_prices/proofs/tests.py
@@ -89,63 +89,97 @@ class ProofPropertyTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.location = LocationFactory(**LOCATION_NODE_652825274)
-        cls.proof = ProofFactory(
+        cls.proof_price_tag = ProofFactory(
             type=proof_constants.TYPE_PRICE_TAG,
             location_osm_id=cls.location.osm_id,
             location_osm_type=cls.location.osm_type,
         )
         PriceFactory(
-            proof_id=cls.proof.id,
+            proof_id=cls.proof_price_tag.id,
             location_osm_id=cls.location.osm_id,
             location_osm_type=cls.location.osm_type,
             price=1.0,
         )
         PriceFactory(
-            proof_id=cls.proof.id,
+            proof_id=cls.proof_price_tag.id,
             location_osm_id=cls.location.osm_id,
             location_osm_type=cls.location.osm_type,
             price=2.0,
         )
+        cls.proof_receipt = ProofFactory(type=proof_constants.TYPE_RECEIPT)
+        PriceFactory(
+            proof_id=cls.proof_receipt.id,
+            location_osm_id=cls.location.osm_id,
+            location_osm_type=cls.location.osm_type,
+            price=2.0,
+            currency="EUR",
+            date="2024-06-30",
+        )
 
     def test_is_type_single_shop(self):
-        self.assertTrue(self.proof.is_type_single_shop)
+        self.assertTrue(self.proof_price_tag.is_type_single_shop)
 
     def test_update_price_count(self):
-        self.proof.refresh_from_db()
-        self.assertEqual(self.proof.price_count, 2)  # price post_save
+        self.proof_price_tag.refresh_from_db()
+        self.assertEqual(self.proof_price_tag.price_count, 2)  # price post_save
         # bulk delete prices to skip signals
-        self.proof.prices.all().delete()
-        self.assertEqual(self.proof.price_count, 2)  # should be 0
+        self.proof_price_tag.prices.all().delete()
+        self.assertEqual(self.proof_price_tag.price_count, 2)  # should be 0
         # update_price_count() should fix price_count
-        self.proof.update_price_count()
-        self.assertEqual(self.proof.price_count, 0)  # all deleted
+        self.proof_price_tag.update_price_count()
+        self.assertEqual(self.proof_price_tag.price_count, 0)  # all deleted
 
     def test_update_location(self):
         # existing
-        self.proof.refresh_from_db()
+        self.proof_price_tag.refresh_from_db()
         self.location.refresh_from_db()
-        self.assertEqual(self.proof.price_count, 2)
-        self.assertEqual(self.proof.location.id, self.location.id)
-        self.assertEqual(self.location.price_count, 2)
+        self.assertEqual(self.proof_price_tag.price_count, 2)
+        self.assertEqual(self.proof_price_tag.location.id, self.location.id)
+        self.assertEqual(self.location.price_count, 2 + 1)
         # update location
-        self.proof.update_location(
+        self.proof_price_tag.update_location(
             location_osm_id=6509705997,
             location_osm_type=location_constants.OSM_TYPE_NODE,
         )
         # check changes
-        self.proof.refresh_from_db()
+        self.proof_price_tag.refresh_from_db()
         self.location.refresh_from_db()
-        new_location = self.proof.location
+        new_location = self.proof_price_tag.location
         self.assertNotEqual(self.location, new_location)
-        self.assertEqual(self.proof.price_count, 2)
+        self.assertEqual(self.proof_price_tag.price_count, 2)
         self.assertEqual(new_location.price_count, 2)
-        self.assertEqual(self.location.price_count, 0)
+        self.assertEqual(self.location.price_count, 3 - 2)
         # update again, same location
-        self.proof.update_location(
+        self.proof_price_tag.update_location(
             location_osm_id=6509705997,
             location_osm_type=location_constants.OSM_TYPE_NODE,
         )
-        self.proof.refresh_from_db()
+        self.proof_price_tag.refresh_from_db()
         self.location.refresh_from_db()
-        self.assertEqual(self.proof.price_count, 2)
-        self.assertEqual(self.proof.location.price_count, 2)
+        self.assertEqual(self.proof_price_tag.price_count, 2)
+        self.assertEqual(self.proof_price_tag.location.price_count, 2)
+
+    def test_set_missing_location_from_prices(self):
+        self.proof_receipt.refresh_from_db()
+        self.assertTrue(self.proof_receipt.location is None)
+        self.assertEqual(self.proof_receipt.price_count, 1)
+        self.proof_receipt.set_missing_location_from_prices()
+        self.assertEqual(self.proof_receipt.location, self.location)
+
+    def test_set_missing_date_from_prices(self):
+        self.proof_receipt.refresh_from_db()
+        self.assertTrue(self.proof_receipt.date is None)
+        self.assertEqual(self.proof_receipt.price_count, 1)
+        self.proof_receipt.set_missing_date_from_prices()
+        self.assertEqual(
+            self.proof_receipt.date, self.proof_receipt.prices.first().date
+        )
+
+    def test_set_missing_currency_from_prices(self):
+        self.proof_receipt.refresh_from_db()
+        self.assertTrue(self.proof_receipt.currency is None)
+        self.assertEqual(self.proof_receipt.price_count, 1)
+        self.proof_receipt.set_missing_currency_from_prices()
+        self.assertEqual(
+            self.proof_receipt.currency, self.proof_receipt.prices.first().currency
+        )
