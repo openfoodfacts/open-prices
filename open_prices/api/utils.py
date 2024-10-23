e49f773716da0b7cb99bdf67d8634f39bee56d19
Raphael Odini <raphodn@users.noreply.github.com>
2024-10-24 00:05:01 +0200

    refactor(API): store app_version in Price & Proof source field

--- open_prices/api/utils.py
+++ open_prices/api/utils.py
@@ -6,3 +6,11 @@ def get_object_or_drf_404(model, **kwargs):
         return model.objects.get(**kwargs)
     except model.DoesNotExist:
         raise Http404(f"No {model._meta.verbose_name} matches the given query.")
+
+
+def get_source_from_request(request):
+    app_name = request.GET.get("app_name", "API")
+    app_version = request.GET.get("app_version", "")
+    if app_version:
+        return f"{app_name} ({app_version})"
+    return app_name
