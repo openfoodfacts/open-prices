118a2fb94d1894198beb6dfa784b6337e3b44344
Raphael Odini <raphodn@users.noreply.github.com>
2024-11-26 15:49:53 +0100

    Improve Location & Proof admin pages

--- open_prices/proofs/admin.py
+++ open_prices/proofs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from open_prices.proofs.models import Proof
 
@@ -6,8 +8,9 @@ from open_prices.proofs.models import Proof
 @admin.register(Proof)
 class ProofAdmin(admin.ModelAdmin):
     list_display = (
+        "id",
         "type",
-        "location",
+        "location_with_link",
         "date",
         "currency",
         "price_count",
@@ -15,3 +18,12 @@ class ProofAdmin(admin.ModelAdmin):
         "created",
     )
     list_filter = ("type",)
+
+    def location_with_link(self, proof):
+        if proof.location:
+            url = reverse("admin:locations_location_change", args=[proof.location_id])
+            return format_html(f'<a href="{url}">{proof.location}</a>')
+
+    location_with_link.short_description = Proof._meta.get_field(
+        "location"
+    ).verbose_name
