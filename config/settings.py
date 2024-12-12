824a3895ecd058b6539ef8e5d99f016a1de8bbbb
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2024-12-12 17:04:50 +0100

    chore: update redis default stream name

--- config/settings.py
+++ config/settings.py
@@ -310,7 +310,7 @@ ANONYMOUS_USER_ID = "openfoodfacts-contributors"
 # ------------------------------------------------------------------------------
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
-REDIS_STREAM_NAME = os.getenv("REDIS_STREAM_NAME", "product_updates_off")
+REDIS_STREAM_NAME = os.getenv("REDIS_STREAM_NAME", "product_updates")
 REDIS_LATEST_ID_KEY = os.getenv(
     "REDIS_LATEST_ID_KEY", "open-prices:product_updates:latest_id"
 )
