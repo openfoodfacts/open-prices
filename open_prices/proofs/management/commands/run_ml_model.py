06216c5ccee44533efd5c5dd3460879c36512e95
Raphaël Bournhonesque <raphael0202@users.noreply.github.com>
2024-12-20 11:09:18 +0100

    fix: add a command to extract from Gemini from price tag without prediction (#644)

--- open_prices/proofs/management/commands/run_ml_model.py
+++ open_prices/proofs/management/commands/run_ml_model.py
@@ -5,12 +5,14 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 from openfoodfacts.utils import get_logger
 
+from open_prices.proofs.constants import PRICE_TAG_EXTRACTION_TYPE, TYPE_PRICE_TAG
 from open_prices.proofs.ml import (
     PRICE_TAG_DETECTOR_MODEL_NAME,
     PROOF_CLASSIFICATION_MODEL_NAME,
+    run_and_save_price_tag_extraction,
     run_and_save_proof_prediction,
 )
-from open_prices.proofs.models import Proof
+from open_prices.proofs.models import PriceTagPrediction, Proof
 
 # Initializing root logger
 get_logger()
@@ -19,7 +21,11 @@ get_logger()
 class Command(BaseCommand):
     help = """Run ML models on images with proof predictions, and save the predictions
     in DB."""
-    _allowed_types = ["proof_classification", "price_tag_detection"]
+    _allowed_types = [
+        "proof_classification",
+        "price_tag_detection",
+        "price_tag_extraction",
+    ]
 
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
         parser.add_argument(
@@ -49,6 +55,13 @@ class Command(BaseCommand):
                 f"Invalid type(s) provided: '{types}', allowed: {self._allowed_types}"
             )
 
+        if "proof_classification" in types or "price_tag_detection" in types:
+            self.handle_proof_prediction_job(types, limit)
+
+        if "price_tag_extraction" in types:
+            self.handle_price_tag_extraction_job(limit)
+
+    def handle_proof_prediction_job(self, types: list[str], limit: int) -> None:
         exclusion_filters_list = []
         if "proof_classification" in types:
             exclusion_filters_list.append(
@@ -82,3 +95,22 @@ class Command(BaseCommand):
             self.stdout.write(f"Processing proof {proof.id}...")
             run_and_save_proof_prediction(proof.id)
             self.stdout.write("Done.")
+
+    def handle_price_tag_extraction_job(self, limit: int) -> None:
+        # Get all proofs of type PRICE_TAG
+        proofs = Proof.objects.filter(type=TYPE_PRICE_TAG).order_by("-id")
+
+        added = 0
+        for proof in tqdm.tqdm(proofs):
+            for price_tag in proof.price_tags.all():
+                # Check if the price tag already has a prediction
+                if not PriceTagPrediction.objects.filter(
+                    type=PRICE_TAG_EXTRACTION_TYPE, price_tag=price_tag
+                ).exists():
+                    self.stdout.write(
+                        f"Processing price tag {price_tag.id} (proof {proof.id})..."
+                    )
+                    run_and_save_price_tag_extraction([price_tag], proof)
+                    added += 1
+                    if limit and added >= limit:
+                        return
