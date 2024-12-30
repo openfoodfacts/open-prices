96215f4460214e2d647bd7ab82039bdf3003f564
Raphaël Odini <raphodn@users.noreply.github.com>
2024-12-30 13:37:27 +0100

    refactor(API): store query params app_platform & app_page in source field (#658)

--- open_prices/api/utils.py
+++ open_prices/api/utils.py
@@ -11,6 +11,12 @@ def get_object_or_drf_404(model, **kwargs):
 def get_source_from_request(request):
     app_name = request.GET.get("app_name", "API")
     app_version = request.GET.get("app_version", "")
+    app_platform = request.GET.get("app_platform", "")
+    app_page = request.GET.get("app_page", "")
     if app_version:
-        return f"{app_name} ({app_version})"
+        app_name += f" ({app_version})"
+    if app_platform:
+        app_name += f" ({app_platform})"
+    if app_page:
+        app_name += f" - {app_page}"
     return app_name
