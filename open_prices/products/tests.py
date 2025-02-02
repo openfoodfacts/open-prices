51bf3fc0aac93893aa3143d213d95c0eb368cfbc
Raphael Odini <raphodn@users.noreply.github.com>
2025-02-02 18:12:46 +0100

    Update tests

--- open_prices/products/tests.py
+++ open_prices/products/tests.py
@@ -57,6 +57,13 @@ LOCATION_OSM_NODE_652825274 = {
     "osm_name": "Monoprix",
     "osm_lat": "45.1805534",
     "osm_lon": "5.7153387",
+    "osm_address_city": "Grenoble",
+    "osm_address_country": "France",
+    "price_count": 15,
+}
+LOCATION_ONLINE_DECATHLON = {
+    "type": location_constants.TYPE_ONLINE,
+    "website_url": "https://www.decathlon.fr",
     "price_count": 15,
 }
 
@@ -112,27 +119,31 @@ class ProductPropertyTest(TestCase):
     def setUpTestData(cls):
         cls.product = ProductFactory(code="0123456789100", product_quantity=1000)
         cls.user = UserFactory()
-        cls.location = LocationFactory(**LOCATION_OSM_NODE_652825274)
+        cls.location_1 = LocationFactory(**LOCATION_OSM_NODE_652825274)
+        cls.location_2 = LocationFactory(**LOCATION_ONLINE_DECATHLON)
         cls.proof = ProofFactory(
-            location_osm_id=cls.location.osm_id,
-            location_osm_type=cls.location.osm_type,
+            location_osm_id=cls.location_1.osm_id,
+            location_osm_type=cls.location_1.osm_type,
+            currency="EUR",
             owner=cls.user.user_id,
         )
         PriceFactory(
             product_code=cls.product.code,
-            location_osm_id=cls.location.osm_id,
-            location_osm_type=cls.location.osm_type,
+            location_osm_id=cls.location_1.osm_id,
+            location_osm_type=cls.location_1.osm_type,
             proof_id=cls.proof.id,
             price=1.0,
+            currency=cls.proof.currency,
             price_is_discounted=True,
             price_without_discount=1.5,
             owner=cls.user.user_id,
         )
         PriceFactory(
             product_code=cls.product.code,
-            location_osm_id=cls.location.osm_id,
-            location_osm_type=cls.location.osm_type,
+            location_osm_id=cls.location_2.osm_id,
+            location_osm_type=cls.location_2.osm_type,
             price=2.0,
+            currency="USD",
             owner=cls.user.user_id,
         )
 
@@ -170,32 +181,41 @@ class ProductPropertyTest(TestCase):
 
     def test_update_price_count(self):
         self.product.refresh_from_db()
-        self.assertEqual(self.product.price_count, 2)  # price post_save
+        self.assertEqual(self.product.price_count, 2)  # price signals
+        self.assertEqual(self.product.price_currency_count, 0)
+        # update_price_count() should fix price counts
+        self.product.update_price_count()
+        self.assertEqual(self.product.price_count, 2)
+        self.assertEqual(self.product.price_currency_count, 2)
         # bulk delete prices to skip signals
         self.product.prices.all().delete()
         self.assertEqual(self.product.price_count, 2)  # should be 0
-        # update_price_count() should fix price_count
+        self.assertEqual(self.product.price_currency_count, 2)  # should be 0
+        # update_price_count() should fix price counts
         self.product.update_price_count()
         self.assertEqual(self.product.price_count, 0)  # all deleted
+        self.assertEqual(self.product.price_currency_count, 0)
 
     def test_update_location_count(self):
         self.product.refresh_from_db()
         self.assertEqual(self.product.location_count, 0)
-        # update_location_count() should fix location_count
+        self.assertEqual(self.product.location_type_osm_country_count, 0)
+        # update_location_count() should fix location counts
         self.product.update_location_count()
         self.assertEqual(self.product.location_count, 1)
+        self.assertEqual(self.product.location_type_osm_country_count, 1)
 
     def test_update_user_count(self):
         self.product.refresh_from_db()
         self.assertEqual(self.product.user_count, 0)
-        # update_user_count() should fix user_count
+        # update_user_count() should fix user counts
         self.product.update_user_count()
         self.assertEqual(self.product.user_count, 1)
 
     def test_update_proof_count(self):
         self.product.refresh_from_db()
         self.assertEqual(self.product.proof_count, 0)
-        # update_proof_count() should fix proof_count
+        # update_proof_count() should fix proof counts
         self.product.update_proof_count()
         self.assertEqual(self.product.proof_count, 1)
 
