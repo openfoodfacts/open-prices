eb719a32f76a65b48937445e12d2984e371f9564
Alex Bourreau <7023229+TTalex@users.noreply.github.com>
2025-03-08 18:01:41 +0100

    docs(API): custom paginated response schema (#751)
    Co-authored-by: Raphael Odini <raphodn@users.noreply.github.com>

--- open_prices/api/pagination.py
+++ open_prices/api/pagination.py
@@ -1,14 +1,14 @@
-from rest_framework import pagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
-class CustomPagination(pagination.PageNumberPagination):
+class CustomPagination(PageNumberPagination):
     """
-    https://www.django-rest-framework.org/api-guide/pagination/#custom-pagination-styles
-    - results -> items
-    - count -> total
-    - next -> ?
-    - previous -> ?
+    docs: https://www.django-rest-framework.org/api-guide/pagination/#custom-pagination-styles  # noqa
+    why do we override the pagination keys? we used to have fastapi-pagination before  # noqa
+    - overriden keys: results -> items; count -> total
+    - added keys: page, pages, size
+    - removed keys: next, previous
     """
 
     page_size_query_param = "size"
@@ -23,3 +23,35 @@ class CustomPagination(pagination.PageNumberPagination):
                 "total": self.page.paginator.count,
             }
         )
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            "type": "object",
+            "required": ["items", "page", "pages", "size", "total"],
+            "properties": {
+                "items": {
+                    "type": "array",
+                    "items": schema,
+                },
+                "page": {
+                    "type": "integer",
+                    "description": "Current page number",
+                    "example": 1,
+                },
+                "pages": {
+                    "type": "integer",
+                    "description": "Total number of pages",
+                    "example": 16,
+                },
+                "size": {
+                    "type": "integer",
+                    "description": "Number of items per page",
+                    "example": 100,
+                },
+                "total": {
+                    "type": "integer",
+                    "description": "Total number of items",
+                    "example": 1531,
+                },
+            },
+        }
