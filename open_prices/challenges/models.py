233e028c28b30e5eb4c8cb2872ca5c005f1de3a8
Raphael Odini <raphodn@users.noreply.github.com>
2025-03-17 11:38:00 +0100

    Add tests on start & end date

--- open_prices/challenges/models.py
+++ open_prices/challenges/models.py
@@ -1,8 +1,10 @@
 from django.contrib.postgres.fields import ArrayField
+from django.core.validators import ValidationError
 from django.db import models
 from django.utils import timezone
 
 from open_prices.challenges import constants as challenge_constants
+from open_prices.common import utils
 
 
 class Challenge(models.Model):
@@ -27,3 +29,20 @@ class Challenge(models.Model):
         db_table = "challenges"
         verbose_name = "Challenge"
         verbose_name_plural = "Challenges"
+
+    def clean(self, *args, **kwargs):
+        validation_errors = dict()
+        # date rules
+        if self.start_date is not None and self.end_date is not None:
+            if self.start_date > self.end_date:
+                utils.add_validation_error(
+                    validation_errors, "start_date", "Must be before end date"
+                )
+        # return
+        if bool(validation_errors):
+            raise ValidationError(validation_errors)
+        super().clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
--- open_prices/challenges/tests.py
+++ open_prices/challenges/tests.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from open_prices.challenges.factories import ChallengeFactory
@@ -8,5 +9,15 @@ class ChallengeModelSaveTest(TestCase):
     def setUpTestData(cls):
         pass
 
-    def test_challenge_validation(self):
-        ChallengeFactory()
+    def test_challenge_date_validation(self):
+        ChallengeFactory(start_date=None, end_date=None)
+        ChallengeFactory(start_date=None, end_date="2024-06-30")
+        ChallengeFactory(start_date="2024-06-30", end_date=None)
+        ChallengeFactory(start_date="2024-06-30", end_date="2024-07-05")
+        ChallengeFactory(start_date="2024-06-30", end_date="2024-06-30")
+        self.assertRaises(
+            ValidationError,
+            ChallengeFactory,
+            start_date="2024-06-30",
+            end_date="2024-06-29",
+        )
