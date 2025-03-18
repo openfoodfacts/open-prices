b39ea4215a00c7e0607973fb358e6c3784fb3043
Raphaël Odini <raphodn@users.noreply.github.com>
2025-03-18 10:45:56 +0100

    feat(Moderation): script to remove Products (and their prices) with invalid barcodes (#761)

--- open_prices/moderation/rules.py
+++ open_prices/moderation/rules.py
@@ -1,7 +1,7 @@
 """
 A list of rules, to clean up the data
 """
-
+import openfoodfacts
 from django.db.models import Q
 from django.db.models.functions import Length
 
@@ -11,19 +11,19 @@ from open_prices.products.models import Product
 
 def cleanup_products_with_long_barcodes():
     """
-    Remove products (and their prices) with long barcodes
+    Remove products (and their prices) that have (too) long barcodes
     - long barcode = more than 13 characters
-    - only if the price come from the validation workflows
-    - only from unknown source (aka not from OxF)
+    - only if the product has an unknown source (aka not from OxF)
+    - only if the price came from the validation workflows
     """
     # init
-    price_deleted_count = 0
     product_deleted_count = 0
+    price_deleted_count = 0
 
     # products with long barcodes
     product_queryset = Product.objects.annotate(
         code_length_annotated=Length("code")
-    ).filter(code_length_annotated__gt=13, source=None)
+    ).filter(source=None, code_length_annotated__gt=13)
     print(f"Found {product_queryset.count()} products with long barcodes")
 
     # build the price source filter query
@@ -43,4 +43,47 @@ def cleanup_products_with_long_barcodes():
                 product_deleted_count += 1
 
     # recap
-    print(f"Deleted {price_deleted_count} prices and {product_deleted_count} products")
+    print(f"Deleted {product_deleted_count} products and {price_deleted_count} prices")
+
+
+def cleanup_products_with_invalid_barcodes():
+    """
+    Remove products (and their prices) that have invalid barcodes
+    invalid barcode = invalid check digit
+    - only if the product has an unknown source (aka not from OxF)
+    - only if the price came from the validation workflows
+    """
+    # init
+    product_deleted_count = 0
+    price_deleted_count = 0
+
+    # products with invalid barcodes
+    product_code_list = Product.objects.filter(source=None).values_list(
+        "code", flat=True
+    )
+    product_code_invalid_list = [
+        code
+        for code in product_code_list
+        if not openfoodfacts.barcode.has_valid_check_digit(code)
+    ]
+    product_queryset = Product.objects.filter(code__in=product_code_invalid_list)
+    print(f"Found {product_queryset.count()} products with invalid barcodes")
+
+    # build the price source filter query
+    source_query = Q()
+    for source in price_constants.PRICE_CREATED_FROM_PRICE_TAG_VALIDATION_SOURCE_LIST:
+        source_query |= Q(source__contains=source)
+
+    # loop on each product
+    for product in product_queryset:
+        product_prices_from_source_queryset = product.prices.filter(source_query)
+        if product_prices_from_source_queryset.exists():
+            for price in product_prices_from_source_queryset.all():
+                price.delete()  # delete 1 by 1 to trigger signals
+                price_deleted_count += 1
+            if product.prices.count() == 0:
+                product.delete()
+                product_deleted_count += 1
+
+    # recap
+    print(f"Deleted {product_deleted_count} products and {price_deleted_count} prices")
--- open_prices/moderation/tests.py
+++ open_prices/moderation/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 
-from open_prices.moderation.rules import cleanup_products_with_long_barcodes
+from open_prices.moderation.rules import (
+    cleanup_products_with_invalid_barcodes,
+    cleanup_products_with_long_barcodes,
+)
 from open_prices.prices.factories import PriceFactory
 from open_prices.prices.models import Price
+from open_prices.products import constants as product_constants
 from open_prices.products.factories import ProductFactory
 from open_prices.products.models import Product
 
@@ -10,21 +14,45 @@ from open_prices.products.models import Product
 class ModerationRulesTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.product_ok = ProductFactory(code="0123456789100")  # 13 characters
+        barcode_ok = "8001505005707"
+        cls.product_ok = ProductFactory(
+            code=barcode_ok, source=product_constants.SOURCE_OFF
+        )
         PriceFactory(product_code=cls.product_ok.code, source="mobile")
         PriceFactory(
             product_code=cls.product_ok.code,
             source="web - /experiments/price-validation-assistant",
         )
-        cls.product_not_ok = ProductFactory(code="01234567891000")  # 14 characters
+        barcode_invalid = "0123456789100"
+        cls.product_with_barcode_invalid = ProductFactory(
+            code=barcode_invalid, source=None
+        )
         PriceFactory(
-            product_code=cls.product_not_ok.code,
+            product_code=cls.product_with_barcode_invalid.code, source="mobile"
+        )
+        PriceFactory(
+            product_code=cls.product_with_barcode_invalid.code,
+            source="web - /experiments/price-validation-assistant",
+        )
+        barcode_long_and_invalid = "01234567891000"
+        cls.product_with_barcode_long_and_invalid = ProductFactory(
+            code=barcode_long_and_invalid, source=None
+        )
+        PriceFactory(
+            product_code=cls.product_with_barcode_long_and_invalid.code,
             source="web - /experiments/price-validation-assistant",
         )
 
     def test_cleanup_products_with_long_barcodes(self):
-        self.assertEqual(Product.objects.count(), 2)
-        self.assertEqual(Price.objects.count(), 2 + 1)
+        self.assertEqual(Product.objects.count(), 3)
+        self.assertEqual(Price.objects.count(), 5)
         cleanup_products_with_long_barcodes()
-        self.assertEqual(Product.objects.count(), 1)  # 1 product deleted
-        self.assertEqual(Price.objects.count(), 2)  # 1 price deleted
+        self.assertEqual(Product.objects.count(), 2)  # 1 product deleted
+        self.assertEqual(Price.objects.count(), 4)  # 1 price deleted
+
+    def test_cleanup_products_with_invalid_barcodes(self):
+        self.assertEqual(Product.objects.count(), 3)
+        self.assertEqual(Price.objects.count(), 5)
+        cleanup_products_with_invalid_barcodes()
+        self.assertEqual(Product.objects.count(), 2)  # 1 product deleted
+        self.assertEqual(Price.objects.count(), 3)  # 2 prices deleted
