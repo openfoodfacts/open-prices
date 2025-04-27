407d520ec8a931b417f6611eb175fe0b0431e8b7
Raphael Odini <raphodn@users.noreply.github.com>
2025-04-27 15:07:20 +0200

    feat(Challenges): new tag property. return in the API

--- open_prices/api/challenges/serializers.py
+++ open_prices/api/challenges/serializers.py
@@ -4,7 +4,8 @@ from open_prices.challenges.models import Challenge
 
 
 class ChallengeSerializer(serializers.ModelSerializer):
-    status = serializers.CharField(source="status_annotated", read_only=True)
+    status = serializers.CharField(source="status", read_only=True)
+    tag = serializers.CharField(source="tag", read_only=True)
 
     class Meta:
         model = Challenge
