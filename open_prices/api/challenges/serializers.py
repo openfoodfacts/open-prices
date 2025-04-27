fe42b0c03dfba14ffa112ee94e786c0987a198c9
Raphael Odini <raphodn@users.noreply.github.com>
2025-04-27 15:24:16 +0200

    fix serializer

--- open_prices/api/challenges/serializers.py
+++ open_prices/api/challenges/serializers.py
@@ -4,8 +4,8 @@ from open_prices.challenges.models import Challenge
 
 
 class ChallengeSerializer(serializers.ModelSerializer):
-    status = serializers.CharField(source="status", read_only=True)
-    tag = serializers.CharField(source="tag", read_only=True)
+    status = serializers.CharField(read_only=True)
+    tag = serializers.CharField(read_only=True)
 
     class Meta:
         model = Challenge
