751a1d4077d4834c08a5b4c3cc4dbd7b99d81b96
Raphael Odini <raphodn@users.noreply.github.com>
2025-08-10 21:52:38 +0200

    refactor(Proofs): add admin integration for prediction models

--- open_prices/proofs/admin.py
+++ open_prices/proofs/admin.py
@@ -2,7 +2,165 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from open_prices.proofs.models import Proof
+from open_prices.proofs.models import (
+    PriceTag,
+    PriceTagPrediction,
+    Proof,
+    ProofPrediction,
+    ReceiptItem,
+)
+
+
+@admin.register(PriceTagPrediction)
+class PriceTagPredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "price_tag",
+        "type",
+        "model_name",
+        "model_version",
+        "created",
+    )
+    list_filter = ("type",)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class PriceTagPredictionInline(admin.TabularInline):
+    model = PriceTagPrediction
+    extra = 0
+    fields = ("price_tag", "type", "model_name", "model_version", "created")
+    can_delete = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(PriceTag)
+class PriceTagAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "created",
+    )
+    list_filter = ("status",)
+    inlines = (PriceTagPredictionInline,)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class PriceTagInline(admin.TabularInline):
+    model = PriceTag
+    extra = 0
+    fields = ("status", "created")
+    can_delete = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(ReceiptItem)
+class ReceiptItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "created",
+    )
+    list_filter = ("status",)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class ReceiptItemInline(admin.TabularInline):
+    model = ReceiptItem
+    extra = 0
+    fields = ("status", "created")
+    can_delete = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(ProofPrediction)
+class ProofPredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "proof",
+        "type",
+        "model_name",
+        "model_version",
+        "created",
+    )
+    list_filter = ("type",)
+    inlines = (PriceTagInline, ReceiptItemInline)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class ProofPredictionInline(admin.TabularInline):
+    model = ProofPrediction
+    extra = 0
+    fields = ("proof", "type", "model_name", "model_version", "created")
+    can_delete = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Proof)
@@ -18,6 +176,8 @@ class ProofAdmin(admin.ModelAdmin):
         "created",
     )
     list_filter = ("type",)
+    readonly_fields = ("created", "updated")
+    inlines = (ProofPredictionInline,)
 
     def location_with_link(self, proof):
         if proof.location:
