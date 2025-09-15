1526e689c37e0e3c130fae3b7aa2a800ea9b4397
Raphael Odini <raphodn@users.noreply.github.com>
2025-09-15 17:20:45 +0200

    Add to admin

--- open_prices/challenges/admin.py
+++ open_prices/challenges/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from open_prices.challenges.models import Challenge
 
@@ -10,6 +11,7 @@ class ChallengeAdmin(admin.ModelAdmin):
         "start_date",
         "end_date",
         "categories",
+        "location_count",
         "is_published",
         "status_display",
         "created",
@@ -17,7 +19,14 @@ class ChallengeAdmin(admin.ModelAdmin):
     )
     list_filter = ("is_published",)
     search_fields = ("id",)
-    readonly_fields = ("stats", "created", "updated")
+    readonly_fields = ("location_count", "stats", "created", "updated")
+
+    def get_queryset(self, request):
+        return Challenge.objects.annotate(location_count=Count("locations"))
+
+    @admin.display(description="Locations")
+    def location_count(self, obj):
+        return obj.location_count  # annotated
 
     @admin.display(description="Status")
     def status_display(self, obj):
