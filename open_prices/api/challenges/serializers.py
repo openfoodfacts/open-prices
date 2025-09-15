2efc0e61496a64d6c8087146bc2113cff682d933
Raphael Odini <raphodn@users.noreply.github.com>
2025-09-15 17:59:11 +0200

    Update serializer

--- open_prices/api/challenges/serializers.py
+++ open_prices/api/challenges/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 
+from open_prices.api.locations.serializers import LocationSerializer
 from open_prices.challenges.models import Challenge
 
 
 class ChallengeSerializer(serializers.ModelSerializer):
-    status = serializers.CharField(read_only=True)
-    tag = serializers.CharField(read_only=True)
+    locations = LocationSerializer(many=True, read_only=True)
+    status = serializers.CharField(read_only=True)  # property
+    tag = serializers.CharField(read_only=True)  # property
 
     class Meta:
         model = Challenge
