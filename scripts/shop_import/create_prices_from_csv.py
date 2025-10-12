2082755aeae8a2395f149baf2fc039ef96f5bd1f
Raphael Odini <raphodn@users.noreply.github.com>
2025-10-12 23:38:20 +0200

    refactor(Shop Import): move config data to a JSON mapping for reuse

--- scripts/shop_import/create_prices_from_csv.py
+++ scripts/shop_import/create_prices_from_csv.py
@@ -3,74 +3,119 @@ import sys
 import time
 
 from open_prices.common import openfoodfacts as common_openfoodfacts
-from scripts.utils import create_price, read_csv
+from scripts.utils import create_price, is_valid_date, read_csv, read_json
 
 OPEN_PRICES_CREATE_PRICE_ENDPOINT = f'{os.environ.get("API_ENDPOINT")}/prices'
 OPEN_PRICES_TOKEN = os.environ.get("API_TOKEN")
 
 REQUIRED_ENV_PARAMS = [
-    # "FILEPATH"
-    # "DELIMITER" (optional)
-    "PRODUCT_CODE_FIELD",
-    "PRODUCT_NAME_FIELD",
-    "PRICE_FIELD",
-    "CURRENCY",
-    "LOCATION_OSM_ID",
-    "LOCATION_OSM_TYPE",
-    "DATE",
-    "PROOF_ID",
+    "FILEPATH",  # path to CSV file
+    # "DELIMITER",  # optional
+    "MAPPING_FILEPATH",
+    "DATE",  # expected format: YYYY-MM-DD
+    "PROOF_ID",  # must be of type 'SHOP_IMPORT' :)
     "API_ENDPOINT",
-    "API_TOKEN",
-    # DRY_RUN
+    "API_TOKEN",  # will be different if .net or .org
+    # DRY_RUN  # optional, set to 'False' to upload
 ]
 
 
-def map_gdpr_price_list_to_open_prices(price_list, extra_data={}):
-    # map source fields to op fields
+def _get_field_from_shop_mapping(shop_price, shop_mapping, open_prices_field):
+    field_mapping = next(
+        (
+            f
+            for f in shop_mapping.get("fields", [])
+            if f["open_prices"] == open_prices_field
+        ),
+        None,
+    )
+    if field_mapping:
+        return shop_price.get(field_mapping["shop"])
+    return None
+
+
+def shop_filter_rules(shop_price_list, shop_mapping):
+    """
+    Rules to skip some prices (on code, name...)
+    """
+    shop_price_list_filtered = list()
+
+    for shop_price in shop_price_list:
+        passes_test = True
+
+        for filter in shop_mapping.get("filters", []):
+            shop_field_value = shop_price.get(filter["shop"])
+            if not shop_field_value or shop_field_value != filter["value"]:
+                passes_test = False
+
+        if passes_test:
+            shop_price_list_filtered.append(shop_price)
+
+    return shop_price_list_filtered
+
+
+def map_shop_price_list_to_open_prices(shop_price_list, shop_mapping):
+    """
+    Map a price list from Shop format to Open Prices format
+    """
     open_prices_price_list = list()
-    for price in price_list:
+
+    for shop_price in shop_price_list:
         open_prices_price = dict()
         # product_name
-        if os.environ.get("PRODUCT_NAME_FIELD"):
-            open_prices_price["product_name"] = price.get(
-                os.environ.get("PRODUCT_NAME_FIELD")
-            )
+        open_prices_price["product_name"] = _get_field_from_shop_mapping(
+            shop_price, shop_mapping, "product_name"
+        )
         # product_code
-        open_prices_price["product_code"] = price.get(
-            os.environ.get("PRODUCT_CODE_FIELD")
+        open_prices_price["product_code"] = _get_field_from_shop_mapping(
+            shop_price, shop_mapping, "product_code"
         )
         # price
-        price_str = price.get(os.environ.get("PRICE_FIELD"))
-        open_prices_price["price"] = (
-            float(price_str.replace(",", ".")) if price_str else None
+        open_prices_price["price"] = _get_field_from_shop_mapping(
+            shop_price, shop_mapping, "price"
         )
+        if open_prices_price["price"]:
+            open_prices_price["price"] = open_prices_price["price"].replace(",", ".")
+        # currency
+        open_prices_price["currency"] = shop_mapping.get("shop_currency")
+        # location
+        open_prices_price["location_osm_id"] = shop_mapping.get("shop_location_osm_id")
+        open_prices_price["location_osm_type"] = shop_mapping.get(
+            "shop_location_osm_type"
+        )
+        # date
+        open_prices_price["date"] = os.environ.get("DATE")
+        # proof_id
+        open_prices_price["proof_id"] = os.environ.get("PROOF_ID")
         # print(open_prices_price)
-        open_prices_price_list.append({**open_prices_price, **extra_data})
+        open_prices_price_list.append(open_prices_price)
 
     return open_prices_price_list
 
 
-def filter_rules(op_price_list):
+def open_prices_filter_rules(open_prices_price_list):
     """
     Rules to skip some prices (on code, name...)
     """
-    op_price_list_filtered = list()
+    open_prices_price_list_filtered = list()
 
-    for op_price in op_price_list:
+    for open_prices_price in open_prices_price_list:
         passes_test = True
 
-        if not op_price["product_code"]:
+        if not open_prices_price["product_code"]:
             passes_test = False
-        elif not common_openfoodfacts.barcode_is_valid(op_price["product_code"]):
+        elif not common_openfoodfacts.barcode_is_valid(
+            open_prices_price["product_code"]
+        ):
             passes_test = False
 
-        if not op_price["price"]:
+        if not open_prices_price["price"]:
             passes_test = False
 
         if passes_test:
-            op_price_list_filtered.append(op_price)
+            open_prices_price_list_filtered.append(open_prices_price)
 
-    return op_price_list_filtered
+    return open_prices_price_list_filtered
 
 
 if __name__ == "__main__":
@@ -79,54 +124,49 @@ if __name__ == "__main__":
     > FILEPATH= poetry run python scripts/shop_import/create_prices_from_csv.py
     Required params: see REQUIRED_ENV_PARAMS
     """
-    # Step 1: read input file
-    if not os.environ.get("FILEPATH"):
-        sys.exit("Error: missing FILEPATH env")
-    filepath = os.environ.get("FILEPATH")
-    print(f"===== Reading {filepath}")
-    if os.environ.get("DELIMITER"):
-        price_list = read_csv(filepath, delimiter=os.environ.get("DELIMITER"))
-    else:
-        price_list = read_csv(filepath)
-    print(len(price_list))
-
-    print("===== Input example:")
-    print(price_list[0])
-
-    # Step 2: check env params are all present
-    print("===== Checking env params")
+    print("Step 1.1/5: Checking env params")
     for env_param in REQUIRED_ENV_PARAMS:
         if not os.environ.get(env_param):
             sys.exit(f"Error: missing {env_param} env")
+        if env_param == "DATE":
+            if not is_valid_date(os.environ.get("DATE")):
+                sys.exit("Error: DATE env not valid (expected format: YYYY-MM-DD)")
     print("All good :)")
 
-    # Step 3: transform input into OP format
-    print("===== Mapping source file to Open Prices format")
-    source = os.environ.get("SOURCE")
-    extra_data = {
-        "currency": os.environ.get("CURRENCY"),
-        "location_osm_id": int(os.environ.get("LOCATION_OSM_ID")),
-        "location_osm_type": os.environ.get("LOCATION_OSM_TYPE"),
-        "date": os.environ.get("DATE"),
-        # proof_id must be of type "SHOP_IMPORT" :)
-        "proof_id": int(os.environ.get("PROOF_ID")),
-    }
-    open_prices_price_list = map_gdpr_price_list_to_open_prices(
-        price_list, extra_data=extra_data
+    filepath = os.environ.get("FILEPATH")
+    print(f"Step 1.2/5: Reading {filepath}")
+    if os.environ.get("DELIMITER"):
+        shop_price_list = read_csv(filepath, delimiter=os.environ.get("DELIMITER"))
+    else:
+        shop_price_list = read_csv(filepath)
+    print(len(shop_price_list))
+
+    print("Input example:")
+    print(shop_price_list[0])
+
+    shop_mapping_filepath = os.environ.get("MAPPING_FILEPATH")
+    print(f"Step 1.3/5: Reading {shop_mapping_filepath}")
+    shop_mapping = read_json(shop_mapping_filepath)
+
+    print("Step 2/5: Applying shop filtering rules")
+    shop_price_list_filtered = shop_filter_rules(shop_price_list, shop_mapping)
+    print(len(shop_price_list_filtered))
+
+    print("Step 3/5: Mapping source file to Open Prices format")
+    open_prices_price_list = map_shop_price_list_to_open_prices(
+        shop_price_list_filtered, shop_mapping
     )
     print(len(open_prices_price_list))
 
-    # Step 4: filter prices depending on specific rules
-    print("===== Applying source filtering rules")
-    open_prices_price_list_filtered = filter_rules(open_prices_price_list)
+    print("Step 4/5: Applying open_prices filtering rules")
+    open_prices_price_list_filtered = open_prices_filter_rules(open_prices_price_list)
     print(len(open_prices_price_list_filtered))
 
-    print("===== Output example (extra fields will be ignored)")
+    print("Output example (extra fields will be ignored)")
     print(open_prices_price_list_filtered[0])
 
-    # Step 5: send prices to backend via API
     if os.environ.get("DRY_RUN") == "False":
-        print(f"===== Uploading data to {os.environ.get('API_ENDPOINT')}")
+        print(f"Step 5/5: Uploading data to {os.environ.get('API_ENDPOINT')}")
         progress = 0
         for index, price in enumerate(open_prices_price_list_filtered):
             create_price(
@@ -139,4 +179,4 @@ if __name__ == "__main__":
             if (progress % 50) == 0:
                 print(f"{progress}/{len(open_prices_price_list_filtered)}...")
     else:
-        sys.exit("===== No prices uploaded (DRY_RUN env missing or set to 'True')")
+        sys.exit("Step 5/5: No prices uploaded (DRY_RUN env missing or set to 'True')")
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,6 @@
 import csv
+import json
+from datetime import datetime
 
 import requests
 
@@ -7,6 +9,30 @@ def read_csv(filepath, delimiter=","):
     with open(filepath, newline="") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=delimiter)
         return list(reader)
+
+
+def read_json(filepath):
+    with open(filepath) as jsonfile:
+        return json.load(jsonfile)
+
+
+def is_valid_date(date_str):
+    """
+    - check if string
+    - check if it has the format YYYY-MM-DD
+    - check if valid ISO 8601 format
+    """
+    if not isinstance(date_str, str):
+        return False
+    if len(date_str) != 10:
+        return False
+    if date_str[4] != "-" or date_str[7] != "-":
+        return False
+    try:
+        datetime.fromisoformat(date_str)
+    except ValueError:
+        return False
+    return True
 
 
 def create_price(price, API_ENDPOINT, API_TOKEN):
