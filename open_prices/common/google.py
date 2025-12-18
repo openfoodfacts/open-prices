423630a55166e3dbba3ac9aa9a723b86926e9ae1
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2025-12-18 18:45:10 +0100

    fix(ml): close the genai.Client after using it
    It looks like we're not the only one having networking issues with
    google-genai: https://github.com/pydantic/pydantic-ai/issues/3071.
    
    The lib now explicitely advices to close the connection:
    https://github.com/googleapis/python-genai?tab=readme-ov-file#close-a-client
    
    That's what this PR is about:
    
    - close the underlying HTTP connection using a with block
    - don't reuse the client, create a new one each time
    - update the Credentials object directly, instead of letting Google
      creating a new one every-time. This is similar to what is done in
      https://ai.pydantic.dev/models/google/#service-account
    
    For reference, we need to provide the `project` to Google genai.Client
    now, as otherwise it ignores the credentials we provided and tries
    to find credential files at the default location.
    
    I also upgraded google-genai, as new versions fixed some of the
    networking issues that were brought up in the issue tracker.
    
    At first, I started to integrate pydantic-ai, but as pydantic-ai relies
    directly on google-genai for making the requests, let's stick to
    google-genai for now.
    
    This is working locally, let's see of it improves "Network unreachable"
    issues.
    
    toml linter also failed on pyproject.toml, I run it to solve the issue.

--- open_prices/common/google.py
+++ open_prices/common/google.py
@@ -1,12 +1,12 @@
 import base64
 import json
 import logging
-from functools import lru_cache
+from functools import cache
 from pathlib import Path
 
 from django.conf import settings
-from google import genai
 from google.genai import types
+from google.oauth2 import service_account
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +22,10 @@ GEMINI_MODEL_NAME = "gemini"
 GEMINI_MODEL_VERSION = "gemini-2.5-flash"
 
 
-def check_google_credentials() -> None:
-    """Create Google Application Default Credentials (ADC) from variable if
-    doesn't exist.
+@cache
+def get_google_credentials() -> service_account.Credentials | None:
+    """Return Google Application Default Credentials (ADC) from file or
+    variable.
 
     See
     https://cloud.google.com/docs/authentication/set-up-adc-local-dev-environment#service-account
@@ -33,29 +34,27 @@ def check_google_credentials() -> None:
     credentials_path = Path(
         "~/.config/gcloud/application_default_credentials.json"
     ).expanduser()
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+
     if not credentials_path.is_file():
         if settings.GOOGLE_CREDENTIALS:
             logger.info(
                 "No google credentials found at %s. Creating credentials from GOOGLE_CREDENTIALS.",
                 credentials_path,
             )
-            credentials_path.parent.mkdir(parents=True, exist_ok=True)
-            credentials_base64 = settings.GOOGLE_CREDENTIALS
             credentials = json.loads(
-                base64.b64decode(credentials_base64).decode("utf-8")
+                base64.b64decode(settings.GOOGLE_CREDENTIALS).decode("utf-8")
             )
-            with open(credentials_path, "w") as f:
-                json.dump(credentials, f, indent=4)
+            return service_account.Credentials.from_service_account_info(
+                credentials, scopes=scopes
+            )
         else:
             logger.info(
                 "No google credentials found in environment variable GOOGLE_CREDENTIALS",
             )
-
-
-@lru_cache(maxsize=1)
-def get_genai_client() -> genai.Client:
-    check_google_credentials()
-    return genai.Client()
+            return None
+    else:
+        return service_account.Credentials.from_service_account_file(credentials_path)
 
 
 def get_generation_config(
