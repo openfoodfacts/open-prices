68f16612050bb0f5f9862fac5910c4956b8baca6
Raphael Odini <raphodn@users.noreply.github.com>
2026-04-12 15:35:28 +0200

    add fieldsets. add image display

--- open_prices/challenges/admin.py
+++ open_prices/challenges/admin.py
@@ -1,4 +1,7 @@
+import json
+
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from open_prices.challenges import constants as challenge_constants
 from open_prices.challenges.models import Challenge
@@ -24,35 +27,90 @@ class ChallengeAdmin(admin.ModelAdmin):
         "start_date",
         "end_date",
         "categories",
-        "categories_full_count",
-        "location_count",
+        "categories_full_count_annotated",
+        "location_count_annotated",
         "is_published",
-        "status_display",
+        "status_annotated",
         "created",
         "updated",
     )
     list_filter = ("is_published", StatusFilter)
     search_fields = ("id",)
     readonly_fields = (
+        "example_proof_url_display",
         "categories_full",
-        "categories_full_count",
-        "location_count",
+        "categories_full_count_annotated",
+        "location_count_annotated",
+        "status_annotated",
         "stats",
+        "stats_pretty",
         "created",
         "updated",
+    )
+
+    fieldsets = (
+        (
+            "",
+            {
+                "fields": (
+                    "title",
+                    "icon",
+                    "subtitle",
+                    "example_proof_url",
+                    "example_proof_url_display",
+                )
+            },
+        ),
+        ("Dates", {"fields": ("start_date", "end_date")}),
+        (
+            "Categories",
+            {
+                "fields": (
+                    "categories",
+                    "categories_full",
+                    "categories_full_count_annotated",
+                )
+            },
+        ),
+        (
+            "Locations",
+            {
+                "fields": (
+                    "locations",
+                    "location_count_annotated",
+                )
+            },
+        ),
+        ("Status", {"fields": ("is_published", "status_annotated")}),
+        ("Stats", {"fields": ("stats_pretty",)}),
+        ("Metadata", {"fields": ("created", "updated")}),
     )
 
     def get_queryset(self, request):
         return Challenge.objects.with_extra_fields()
 
+    @admin.display(description="Image")
+    def example_proof_url_display(self, obj):
+        if obj.example_proof_url:
+            return mark_safe(
+                f'<a href="{obj.example_proof_url}" target="_blank">'
+                f'<img src="{obj.example_proof_url}" title="{obj.example_proof_url}" height=300 />'  # noqa
+                f"</a>"
+            )
+
     @admin.display(description="Categories full")
-    def categories_full_count(self, obj):
+    def categories_full_count_annotated(self, obj):
         return obj.categories_full_count_annotated
 
     @admin.display(description="Locations")
-    def location_count(self, obj):
+    def location_count_annotated(self, obj):
         return obj.location_count_annotated
 
     @admin.display(description="Status")
-    def status_display(self, obj):
+    def status_annotated(self, obj):
         return obj.status_annotated
+
+    @admin.display(description="Stats (pretty)")
+    def stats_pretty(self, obj):
+        data = json.dumps(obj.stats, indent=2)
+        return mark_safe(f"<pre>{data}</pre>")
