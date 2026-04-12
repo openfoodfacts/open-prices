64ee8c99547819917e0d5309f394a2cbccb4a42a
Raphael Odini <raphodn@users.noreply.github.com>
2026-04-12 15:21:07 +0200

    move queryset. add status filter

--- open_prices/challenges/admin.py
+++ open_prices/challenges/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from django.db.models import Count, F, Func, IntegerField
 
+from open_prices.challenges import constants as challenge_constants
 from open_prices.challenges.models import Challenge
+
+
+class StatusFilter(admin.SimpleListFilter):
+    title = "Status"
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return challenge_constants.CHALLENGE_STATUS_CHOICES
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(status_annotated=self.value())
+        return queryset
 
 
 @admin.register(Challenge)
@@ -18,7 +31,7 @@ class ChallengeAdmin(admin.ModelAdmin):
         "created",
         "updated",
     )
-    list_filter = ("is_published",)
+    list_filter = ("is_published", StatusFilter)
     search_fields = ("id",)
     readonly_fields = (
         "categories_full",
@@ -30,24 +43,16 @@ class ChallengeAdmin(admin.ModelAdmin):
     )
 
     def get_queryset(self, request):
-        return Challenge.objects.annotate(
-            categories_full_count=Func(
-                F("categories_full"),
-                1,
-                function="array_length",
-                output_field=IntegerField(),
-            ),
-            location_count=Count("locations"),
-        )
+        return Challenge.objects.with_extra_fields()
 
     @admin.display(description="Categories full")
     def categories_full_count(self, obj):
-        return obj.categories_full_count  # annotated
+        return obj.categories_full_count_annotated
 
     @admin.display(description="Locations")
     def location_count(self, obj):
-        return obj.location_count  # annotated
+        return obj.location_count_annotated
 
     @admin.display(description="Status")
     def status_display(self, obj):
-        return obj.status  # property
+        return obj.status_annotated
