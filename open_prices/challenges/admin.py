fe701ccd86b848472f4589f63004b0083167d9d8
Raphael Odini <raphodn@users.noreply.github.com>
2026-04-12 11:43:24 +0200

    add to admin

--- open_prices/challenges/admin.py
+++ open_prices/challenges/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.db.models import Count
+from django.db.models import Count, F, Func, IntegerField
 
 from open_prices.challenges.models import Challenge
 
@@ -11,6 +11,7 @@ class ChallengeAdmin(admin.ModelAdmin):
         "start_date",
         "end_date",
         "categories",
+        "categories_full_count",
         "location_count",
         "is_published",
         "status_display",
@@ -19,10 +20,29 @@ class ChallengeAdmin(admin.ModelAdmin):
     )
     list_filter = ("is_published",)
     search_fields = ("id",)
-    readonly_fields = ("location_count", "stats", "created", "updated")
+    readonly_fields = (
+        "categories_full",
+        "categories_full_count",
+        "location_count",
+        "stats",
+        "created",
+        "updated",
+    )
 
     def get_queryset(self, request):
-        return Challenge.objects.annotate(location_count=Count("locations"))
+        return Challenge.objects.annotate(
+            categories_full_count=Func(
+                F("categories_full"),
+                1,
+                function="array_length",
+                output_field=IntegerField(),
+            ),
+            location_count=Count("locations"),
+        )
+
+    @admin.display(description="Categories full")
+    def categories_full_count(self, obj):
+        return obj.categories_full_count  # annotated
 
     @admin.display(description="Locations")
     def location_count(self, obj):
