6399717d5a1d7c06cf9009e9da553d7bfa43333e
Raphael Odini <raphodn@users.noreply.github.com>
2026-05-14 16:13:39 +0200

    refactor(ML Models): refactor

--- open_prices/proofs/management/commands/run_ml_models.py
+++ open_prices/proofs/management/commands/run_ml_models.py
@@ -20,15 +20,26 @@ from open_prices.proofs.models import PriceTagPrediction, Proof
 get_logger()
 
 
+PROOF_MODELS = [
+    "proof_classification",
+    "proof_price_tag_detection",
+    "proof_receipt_extraction",
+]
+
+PRICE_TAG_MODELS = [
+    "price_tag_extraction",
+]
+ALL_MODELS = PROOF_MODELS + PRICE_TAG_MODELS
+
+
 class Command(BaseCommand):
-    help = """Run ML models on images with proof predictions, and save the predictions
-    in DB."""
-    _allowed_types = [
-        "proof_classification",
-        "price_tag_detection",
-        "price_tag_extraction",
-        "receipt_extraction",
-    ]
+    """
+    Usage:
+    - python manage.py run_ml_models --types proof_classification
+    - python manage.py run_ml_models --types proof_classification,proof_price_tag_detection --limit 10 --delay 300
+    """
+
+    help = "Run ML models on images with proof predictions, and save the predictions in DB."
 
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
         parser.add_argument(
@@ -37,9 +48,8 @@ class Command(BaseCommand):
         parser.add_argument(
             "--types",
             type=str,
-            help="Type of model to run. Supported values are `proof_classification`, "
-            "`price_tag_detection` and `price_tag_extraction`. To pass multiple types, "
-            "separate them with commas (e.g. `proof_classification,price_tag_detection`).",
+            help=f"Type of model to run. Supported values are {', '.join(ALL_MODELS)}. To pass multiple types, "
+            f"separate them with commas (e.g. {','.join(ALL_MODELS[:2])}).",
         )
         parser.add_argument(
             "--delay",
@@ -60,36 +70,30 @@ class Command(BaseCommand):
         if types_str:
             types = types_str.split(",")
         else:
-            types = self._allowed_types
+            types = ALL_MODELS
 
-        if not all(t in self._allowed_types for t in types):
+        if not all(t in ALL_MODELS for t in types):
             raise ValueError(
-                f"Invalid type(s) provided: '{types}', allowed: {self._allowed_types}"
+                f"Invalid type(s) provided: '{types}', allowed: {ALL_MODELS}"
             )
 
-        if (
-            "proof_classification" in types
-            or "price_tag_detection" in types
-            or "receipt_extraction" in types
-        ):
-            self.handle_proof_prediction_job(types, limit, delay)
+        if any(t in types for t in PROOF_MODELS):
+            self.handle_proof_jobs(types, limit, delay)
 
-        if "price_tag_extraction" in types:
-            self.handle_price_tag_extraction_job(limit, delay)
+        if any(t in types for t in PRICE_TAG_MODELS):
+            self.handle_price_tag_jobs(limit, delay)
 
-    def handle_proof_prediction_job(
-        self, types: list[str], limit: int, delay: int
-    ) -> None:
+    def handle_proof_jobs(self, types: list[str], limit: int, delay: int) -> None:
         exclusion_filters_list = []
         if "proof_classification" in types:
             exclusion_filters_list.append(
                 Q(predictions__model_name=proof_classification_model_config.model_name)
             )
-        if "price_tag_detection" in types:
+        if "proof_price_tag_detection" in types:
             exclusion_filters_list.append(
                 Q(predictions__model_name=PRICE_TAG_DETECTOR_MODEL_NAME)
             )
-        if "receipt_extraction" in types:
+        if "proof_receipt_extraction" in types:
             exclusion_filters_list.append(
                 Q(
                     predictions__type=proof_constants.PROOF_PREDICTION_RECEIPT_EXTRACTION_TYPE
@@ -124,11 +128,11 @@ class Command(BaseCommand):
                 proof,
                 run_price_tag_classification=False,
                 run_price_tag_extraction=False,
-                run_receipt_extraction="receipt_extraction" in types,
+                run_receipt_extraction="proof_receipt_extraction" in types,
             )
             self.stdout.write("Done.")
 
-    def handle_price_tag_extraction_job(self, limit: int, delay: int) -> None:
+    def handle_price_tag_jobs(self, limit: int, delay: int) -> None:
         # Get all proofs of type PRICE_TAG
         proofs = Proof.objects.filter(
             created__lt=timezone.now() - datetime.timedelta(seconds=delay),
