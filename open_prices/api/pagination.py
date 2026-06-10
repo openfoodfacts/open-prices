8afcc778a5082da3a2e8d41d780d53a087630508
saivats <saivatsias@gmail.com>
2026-06-10 10:22:07 +0530

    fix: correct OpenAPI spec for paginated endpoints

--- open_prices/api/pagination.py
+++ open_prices/api/pagination.py
@@ -1,3 +1,4 @@
+from drf_spectacular.extensions import OpenApiPaginationExtension
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -56,4 +57,37 @@ class CustomPagination(PageNumberPagination):
                     "example": 1531,
                 },
             },
+        }
+
+
+class CustomPaginationExtension(OpenApiPaginationExtension):
+    target_class = "open_prices.api.pagination.CustomPagination"
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            "type": "object",
+            "properties": {
+                "items": schema,
+                "page": {
+                    "type": "integer",
+                    "description": "Current page number",
+                    "example": 1,
+                },
+                "pages": {
+                    "type": "integer",
+                    "description": "Total number of pages",
+                    "example": 16,
+                },
+                "size": {
+                    "type": "integer",
+                    "description": "Number of items per page",
+                    "example": 100,
+                },
+                "total": {
+                    "type": "integer",
+                    "description": "Total number of items",
+                    "example": 1531,
+                },
+            },
+            "required": ["items", "page", "pages", "size", "total"],
         }
