ed2f158827291493525119d8e360382523f2b088
Raphael Odini <raphodn@users.noreply.github.com>
2026-06-24 18:03:44 +0200

    more tests

--- open_prices/badges/tests.py
+++ open_prices/badges/tests.py
@@ -39,7 +39,7 @@ class BadgePropertyTest(TestCase):
         self.user.save()
         self.assertTrue(self.badge.user_has_achieved(self.user))
 
-    def test_update_user_badges_and_count(self):
+    def test_when_user_meets_threshold_update_user_badges_and_count(self):
         # Initially, the user has 0 prices and no badges
         self.assertEqual(self.user.price_count, 0)
         self.assertEqual(UserBadge.objects.count(), 0)
@@ -53,6 +53,7 @@ class BadgePropertyTest(TestCase):
         # Update user badges and count
         self.badge.update_user_badges()
         self.badge.update_user_count()
+        self.badge.refresh_from_db()
 
         # Stats should be updated correctly
         self.assertEqual(UserBadge.objects.count(), 1)
@@ -63,22 +64,79 @@ class BadgePropertyTest(TestCase):
         ).achieved_at
         self.assertIsNotNone(user_badge_achieved_at)
 
-        # Create another badge + another user who meets both thresholds
-        badge_2 = BadgeFactory(metric=badge_constants.METRIC_PRICE_COUNT, threshold=10)
-        UserFactory(price_count=15)
+    def test_when_user_already_has_badge_do_not_update_achieved_at(self):
+        # Make the user meet the threshold and update badges
+        self.user.price_count = 5
+        self.user.save()
 
-        # Update user badges and count for both badges
-        for badge in [self.badge, badge_2]:
-            badge.update_user_badges()
-            badge.update_user_count()
+        # Update user badges and count
+        self.badge.update_user_badges()
+        self.badge.update_user_count()
+        self.badge.refresh_from_db()
+        user_badge_achieved_at = UserBadge.objects.get(
+            user=self.user, badge=self.badge
+        ).achieved_at
+
+        # Update user badges and count again
+        self.badge.update_user_badges()
+        self.badge.update_user_count()
+        self.badge.refresh_from_db()
+
+        # user_badge_achieved_at is unchanged
+        self.assertEqual(
+            UserBadge.objects.get(user=self.user, badge=self.badge).achieved_at,
+            user_badge_achieved_at,
+        )
+
+    def test_when_user_meets_multiple_thresholds_update_user_badges_and_counts(self):
+        # Create another badge with a different threshold
+        badge_2 = BadgeFactory(metric=badge_constants.METRIC_PRICE_COUNT, threshold=10)
+
+        # Make the user meet the threshold for both badges
+        self.user.price_count = 15
+        self.user.save()
+
+        # Update user badges and count
+        self.badge.update_user_badges()
+        self.badge.update_user_count()
+        badge_2.update_user_badges()
+        badge_2.update_user_count()
+        self.badge.refresh_from_db()
+        badge_2.refresh_from_db()
 
         # Stats should be updated correctly for both badges
-        self.assertEqual(UserBadge.objects.count(), 2 + 1)
-        self.assertEqual(self.badge.user_badges.count(), 2)
-        self.assertEqual(self.badge.user_count, 2)
+        self.assertEqual(UserBadge.objects.count(), 2)
+        self.assertEqual(self.badge.user_badges.count(), 1)
+        self.assertEqual(self.badge.user_count, 1)
         self.assertEqual(badge_2.user_badges.count(), 1)
         self.assertEqual(badge_2.user_count, 1)
-        # user_badge_achieved_at is unchanged
+
+    def test_when_user_does_not_meet_threshold_anymore_badge_is_not_removed(self):
+        # Make the user meet the threshold
+        self.user.price_count = 5
+        self.user.save()
+
+        # Update user badges and count
+        self.badge.update_user_badges()
+        self.badge.update_user_count()
+        self.badge.refresh_from_db()
+        user_badge_achieved_at = UserBadge.objects.get(
+            user=self.user, badge=self.badge
+        ).achieved_at
+
+        # Now, reduce the user's price_count below the threshold
+        self.user.price_count = 3
+        self.user.save()
+
+        # Update user badges and count again
+        self.badge.update_user_badges()
+        self.badge.update_user_count()
+        self.badge.refresh_from_db()
+
+        # The badge should still be present for the user
+        self.assertEqual(
+            UserBadge.objects.filter(user=self.user, badge=self.badge).count(), 1
+        )
         self.assertEqual(
             UserBadge.objects.get(user=self.user, badge=self.badge).achieved_at,
             user_badge_achieved_at,
