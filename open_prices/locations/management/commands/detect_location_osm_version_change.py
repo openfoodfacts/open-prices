19833efc80f84b57482d94342d8da94c7167839d
Raphael Odini <raphodn@users.noreply.github.com>
2026-09-15 00:21:31 +0200

    refactoring

--- open_prices/locations/management/commands/detect_location_osm_version_change.py
+++ open_prices/locations/management/commands/detect_location_osm_version_change.py
@@ -1,4 +1,5 @@
 import time
+from collections import Counter
 
 from django.core.management.base import BaseCommand
 
@@ -7,12 +8,26 @@ from open_prices.locations.models import Location
 from open_prices.locations.tasks import fetch_and_save_data_from_openstreetmap
 
 
+class Status:
+    UNCHANGED = "unchanged"
+    SMALL_CHANGE = "small_change"
+    BIG_CHANGE = "big_change"
+    NOT_FOUND = "not_found"
+    DELETED = "deleted"
+    ERROR = "error"
+
+
 class Command(BaseCommand):
     """
     Usage: python manage.py detect_location_osm_version_change
     """
 
-    help = "Detect Locations that may have changed (rebranded) on OSM, based on their OSM version change. Small changes are fetched & saved automatically; big changes are only reported, for manual review. This command does not create new Locations."
+    help = """
+    Detect Locations that may have changed (rebranded) on OSM, based on their OSM version change.
+    Small changes are fetched & the location is updated.
+    Big changes are only reported, for manual review.
+    This command does not create new Locations.
+    """
 
     def handle(self, *args, **options) -> None:  # type: ignore
         qs = Location.objects.has_type_osm()
@@ -22,60 +37,79 @@ class Command(BaseCommand):
             f"Of which {qs.count()} have their osm_version field filled..."
         )
 
-        has_osm_change_counter = 0
-        has_big_osm_change_counter = 0
-        deleted_counter = 0
+        counters = Counter()
+
         for index, location in enumerate(qs.all()):
             try:
-                # we get the current (latest) version of the location
-                response = common_openstreetmap.get_location_from_osm(
-                    location.osm_id, location.osm_type, history=False
-                )
-                if response:
-                    osm_data = {
-                        "version": response.version(),
-                        "version_date": response.timestamp(),
-                        "name": response.tag("name"),
-                        "brand": response.tag("brand"),
-                        "tag_value": response.tag(location.osm_tag_key),
-                        "lat": response.lat(),
-                        "lon": response.lon(),
-                    }
-                    # has change
-                    if location.osm_version != osm_data["version"]:
-                        self.stdout.write(
-                            f"=== {location.id} / {location.osm_type} / {location.osm_id}"
-                        )
-                        has_osm_change_counter += 1
-                        # big change
-                        if common_openstreetmap.has_big_osm_change(location, osm_data):
-                            has_big_osm_change_counter += 1
-                            for field, new_value in osm_data.items():
-                                old_value = getattr(location, f"osm_{field}")
-                                if old_value != new_value:
-                                    self.stdout.write(
-                                        f"{field}: {old_value} -> {new_value}"
-                                    )
-                        # small change: fetch full data and update the location
-                        else:
-                            fetch_and_save_data_from_openstreetmap(
-                                location, existing_osm_response=response
-                            )
-                            self.stdout.write("Small change, location updated!")
-
-                else:
-                    self.stdout.write(f"Could not find current OSM data for {location}")
-                if index and (index % 100 == 0):
-                    print(f"{index} / {qs.count()}")
-                time.sleep(1)  # be nice to the OSM API
+                status = self._process_location(location)
             except Exception as e:
                 if common_openstreetmap.is_deleted_osm_error(e):
-                    deleted_counter += 1
+                    status = Status.DELETED
                     self.stdout.write(f"=== {location} === deleted on OSM (410 Gone)")
                 else:
+                    status = Status.ERROR
                     self.stdout.write(f"Error with {location}: {e}")
-        self.stdout.write(f"Total locations with OSM changes: {has_osm_change_counter}")
+            counters[status] += 1
+            if index and (index % 100 == 0):
+                print(f"{index} / {qs.count()}")
+            time.sleep(1)  # be nice to the OSM API
+
+        # done!
+        self.stdout.write("=== Summary ===")
+        self.stdout.write(f"Total locations processed: {sum(counters.values())}")
         self.stdout.write(
-            f"Total locations with big OSM changes: {has_big_osm_change_counter}"
+            f"Total locations unchanged on OSM: {counters[Status.UNCHANGED]}"
         )
-        self.stdout.write(f"Total locations deleted on OSM: {deleted_counter}")
+        self.stdout.write(
+            f"Total locations with small OSM changes: {counters[Status.SMALL_CHANGE]}. They have been updated!"
+        )
+        self.stdout.write(
+            f"Total locations with big OSM changes: {counters[Status.BIG_CHANGE]}"
+        )
+        self.stdout.write(
+            f"Total locations not found on OSM: {counters[Status.NOT_FOUND]}"
+        )
+        self.stdout.write(f"Total locations deleted on OSM: {counters[Status.DELETED]}")
+        self.stdout.write(f"Total locations with errors: {counters[Status.ERROR]}")
+
+    def _process_location(self, location) -> str:
+        """
+        Fetch the current OSM data for `location`, compare it to what's
+        stored, and either report a big change or persist a small one.
+
+        Returns one of the Status values.
+        """
+        # we get the current (latest) version of the location
+        response = common_openstreetmap.get_location_from_osm(
+            location.osm_id, location.osm_type, history=False
+        )
+        if not response:
+            self.stdout.write(f"Could not find current OSM data for {location}")
+            return Status.NOT_FOUND
+
+        osm_data = {
+            "version": response.version(),
+            "version_date": response.timestamp(),
+            "name": response.tag("name"),
+            "brand": response.tag("brand"),
+            "lat": response.lat(),
+            "lon": response.lon(),
+            "tag_value": response.tag(location.osm_tag_key),
+        }
+        if location.osm_version == osm_data["version"]:
+            return Status.UNCHANGED
+
+        self.stdout.write(
+            f"=== {location.id} / {location.osm_type} / {location.osm_id}"
+        )
+        if common_openstreetmap.has_big_osm_change(location, osm_data):
+            for field, new_value in osm_data.items():
+                old_value = getattr(location, f"osm_{field}")
+                if old_value != new_value:
+                    self.stdout.write(f"{field}: {old_value} -> {new_value}")
+            return Status.BIG_CHANGE
+
+        # small change: fetch full data and update the location
+        fetch_and_save_data_from_openstreetmap(location, existing_osm_response=response)
+        self.stdout.write("Small change, location updated!")
+        return Status.SMALL_CHANGEB
