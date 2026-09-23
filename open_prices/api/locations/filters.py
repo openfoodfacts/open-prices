66ccff9d5ef48642f181a41d223b2ad45a492bb1
Raphaël Odini <raphodn@users.noreply.github.com>
2026-09-23 16:16:17 +0200

    feat(Locations): API: new filter by osm_tag_key & osm_tag_value (must be provided together) (#1453)

--- open_prices/api/locations/filters.py
+++ open_prices/api/locations/filters.py
@@ -1,6 +1,29 @@
 import django_filters
+from django import forms
 
 from open_prices.locations.models import Location
+
+OSM_TAG_PARAMETER_NAMES = ("osm_tag_key", "osm_tag_value")
+
+
+class LocationFilterForm(forms.Form):
+    def clean(self):
+        cleaned_data = super().clean()
+        provided_parameters = {
+            name for name in OSM_TAG_PARAMETER_NAMES if name in self.data
+        }
+        has_empty_parameter = any(
+            self.data.get(name) in (None, "") for name in provided_parameters
+        )
+        if provided_parameters and (
+            len(provided_parameters) != len(OSM_TAG_PARAMETER_NAMES)
+            or has_empty_parameter
+        ):
+            raise forms.ValidationError(
+                "osm_tag_key and osm_tag_value must be provided together with "
+                "non-empty values."
+            )
+        return cleaned_data
 
 
 class LocationFilter(django_filters.FilterSet):
@@ -22,4 +45,5 @@ class LocationFilter(django_filters.FilterSet):
 
     class Meta:
         model = Location
-        fields = ["type", "osm_type", "price_count"]
+        form = LocationFilterForm
+        fields = ["type", "osm_type", "osm_tag_key", "osm_tag_value", "price_count"]
